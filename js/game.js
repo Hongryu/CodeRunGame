3e544b9884f0af4b2d6d4e2a069466bc97e1f25a
SRin23 <serin4723@naver.com>
2021-11-23 10:34:39 +0900

    serin_inital commit

---
+++ js/game.js
@@ -0,0 +1,36 @@
+const dino = document.querySelector('#dino');
+const cactus = document.querySelector('#cactus')
+const scoreBar = document.querySelector(".scoreBar")
+
+
+let score = 0;
+
+function jump(){
+    if(dino.classList!="jump"){
+        dino.classList.add("jump");
+
+        setTimeout(function(){
+            dino.classList.remove("jump");
+        }, 300);
+        
+    }
+}
+
+let isAlive = setTimeout(function crash(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    if(cactusLeft<50&&cactusLeft>0&&dinoTop>=140){
+        alert("Game Over");
+        score = 0;
+    }
+    setTimeout(crash, 10);
+}, 10);
+
+document.addEventListener("keydown", function(event){
+    jump();
+    changeScore();
+});
+
+function changeScore(){
+    scoreBar.textContent = "점수 : " + ++score
+}
---
+++ js/index.js
@@ -0,0 +1,17 @@
+let startBtn = document.querySelector('#gameStart_btn');
+startBtn.addEventListener('click', function(){
+    
+    window.location.href = '/html/game.html';
+})
+
+let helpBtn = document.querySelector('#gameHelp_btn');
+helpBtn.addEventListener('click', function(){
+    window.location.href = '/html/help.html';
+})
+
+
+let rankBtn = document.querySelector('#ranking_btn');
+
+rankBtn.addEventListener('click', function(){
+    window.location.href = '/html/rank.html';
+})
