388318aadffce87605d5c8ff11d3261d739b44cb
SRin23 <serin4723@naver.com>
2021-11-29 04:01:24 +0900

    player수 체크

---
+++ js/checkPlayer.js
@@ -0,0 +1,9 @@
+let one = document.querySelector("#onePlayer_btn");
+one.addEventListener('click', function(){
+    window.location.href = '../TestFile/Stage1.html';
+})
+
+let two = document.querySelector("#twoPlayer_btn");
+two.addEventListener('click', function(){
+    window.location.href = '../TestFile/Stage1_2person.html';
+})
