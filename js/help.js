f37e9aabf4f70d471ada534fcb54a1c4abdb3dca
SRin23 <serin4723@naver.com>
2021-11-29 01:49:06 +0900

    디테일 수정

--- js/help.js
+++ js/help.js
@@ -1,7 +1,12 @@
-
+let body = document.querySelector("#body");
+body.width=innerWidth;
+body.height=innerHeight;
 
 let backbtn = document.querySelector('#gameBack_btn');
-backbtn.addEventListener('click', function(){
+   backbtn.addEventListener('click', function(){
    history.go(-1);
     
 })
+
+console.log(body.width)
+console.log(body.height)
